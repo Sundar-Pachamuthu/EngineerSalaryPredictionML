71bf729fe4f6b4bb7921d43d20162fa765bc2380
SUNDAR <160745594+Sundar-Pachamuthu@users.noreply.github.com>
2026-04-22 11:29:38 +0530

    Add files via upload

--- EngSalWeb.py
+++ EngSalWeb.py
@@ -113,7 +113,7 @@ elif page == "Career Tracker":
                 for inten in user_data['Category']:
                     if inten == "Intenship":
                         due = user_data[user_data['Category'] == 'Intenship']
-                        st.write(f"### {username} done Intenship for period of {due['Years of Experience'][0]} Years")
+                        st.write(f"### {username} done Intenship for period of {int(due['Years of Experience'])} Years")
                     # else:
                     #     st.write(f"### {username} dosen't")
 
